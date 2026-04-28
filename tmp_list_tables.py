ce589c590a2c05b5098c669979e72e4eecf366bb
Alex Milla <alex@alexmilla.net>
2026-04-29 01:08:16 +0200

    chore: eliminar archivo temporal

--- tmp_list_tables.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-conn = sqlite3.connect('hosting/data/orinsec.db')
-cursor = conn.cursor()
-cursor.execute("SELECT name FROM sqlite_master WHERE type='table' ORDER BY name")
-print('Tables:')
-for r in cursor.fetchall():
-    print(r[0])
-conn.close()
